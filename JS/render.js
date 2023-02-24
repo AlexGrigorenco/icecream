254e1bcff0d2cc191355e1ccf1fef1b812efb954
Donut <grigoaxgrigo@gmail.com>
2023-02-24 11:29:11 +0200

    added how made block

--- JS/render.js
+++ JS/render.js
@@ -56,7 +56,7 @@
     document.querySelector('.products .cards-wrapper').innerHTML = data.map(item => `
                             <div data-item=${item.data} class="card relative max-w-[370px] bg-[${item.bg}] px-[62px] pt-[166px] pb-[60px] flex flex-col gap-[64px] items-center rounded-[24px]">
 
-                            <div style="box-shadow: 0px 8px 30px rgba(10, 136, 23, 0.25);" class="img-block absolute top-[-32%] max-w-[300px] bg-[${item.bg}] rounded-[50%] overflow-hidden">
+                            <div style="box-shadow: 0px 8px 30px rgba(10, 136, 23, 0.25);" class="img-block absolute top-[-32%] max-w-[300px] bg-[${item.bg}] rounded-[50%]">
                             <div class="img-wrapper rounded-[50%] overflow-hidden ">
                                 <img src=${item.image} alt="icecream">
                             </div>
@@ -75,6 +75,44 @@
                             </div>
                             </div>
 
+                        </div>
+    `).join('')
+}
+
+
+//! render HOW MADE   INFO CARDS
+
+{
+    const data = [
+        {
+            img:'./images/how-made/cow-icon.svg',
+            title: '721',
+            text: 'Aliquam ac dui vel dui vulputate consectetur. Mauris massa.',
+        },
+        {
+            img:'./images/how-made/weight-icon.svg',
+            title: '16kg',
+            text: 'Aliquam ac dui vel dui vulputate consectetur. Mauris massa.',
+        },
+        {
+            img:'./images/how-made/icecream-icon.svg',
+            title: '84',
+            text: 'Aliquam ac dui vel dui vulputate consectetur. Mauris massa.',
+        },
+    ]
+
+
+    document.querySelector('.how-made .info-wrapper').innerHTML = data.map(item => `
+                <div class="info-card flex flex-col gap-[16px] items-center max-w-[320px] text-[#FCFCFC]">
+                        <div class="icon-wrapper max-w-[50px] rounded-[50%] bg-[#F8D24E] p-[8px]">
+                            <img src=${item.img} alt="icon">
+                        </div>
+                        <div class="info-title font-[700] text-[58px] leading-[71px] tracking-[0.04em] uppercase pt-[34px]">
+                            ${item.title}
+                        </div>
+                        <p class="text-center font-[400] text-[16px] leading-[176%] tracking-[0.02em]">
+                            ${item.text}
+                        </p>
                         </div>
     `).join('')
 }
